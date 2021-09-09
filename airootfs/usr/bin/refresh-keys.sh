664a9084996b8da2b664066928f6817a3d0182c0
Rafael <85809040+rafaeluy@users.noreply.github.com>
2021-09-09 11:39:51 -0300

    Update refresh-keys.sh

--- airootfs/usr/bin/refresh-keys.sh
+++ airootfs/usr/bin/refresh-keys.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.gedit.desktop', 'gparted.desktop', 'rebornos-install.desktop', 'rebornos-install-2.desktop', 'rebornos-iso-welcome.desktop', 'org.gnome.Terminal.desktop']"
 sudo systemctl enable --now NetworkManager.service
 sudo systemctl enable --now bluetooth.service
